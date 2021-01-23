92d7c69ff102ee065871134495bd4deb14a447dd
Will <will.redington@freyasystems.com>
2021-01-22 20:18:40 -0500

    stuff

--- saas-client/src/component/view/Dashboard/index.tsx
+++ saas-client/src/component/view/Dashboard/index.tsx
@@ -1,32 +1,35 @@
-import SimpleCard from "component/common/Card/SimpleCard";
 import useSocket from "hook/socket";
 import { StateItem } from "model/state";
-import React, { useEffect, useState } from "react";
-import { getStateByName } from "service/state";
-import "react-calendar/dist/Calendar.css";
+import React, { useEffect, useMemo, useState } from "react";
 import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { getStateByName } from "service/state";
 
 export const Dashboard: React.FC = () => {
 	const [socket] = useSocket();
-
-	const [selectedDate, setSelectedDate] = useState(new Date());
 	const [state, setState] = useState<StateItem>({
 		name: "myApp",
 		props: {
-			data: "",
+			data: new Date().toISOString().slice(0, 10),
 		},
 	});
+
+	const selectedDate = useMemo(() => {
+		if ("date" in state.props) {
+			return new Date(state.props["date"]);
+		}
+	}, [state]);
 
 	const handleDateChange = (value: Date | Date[]) => {
 		console.log("value", value);
 		if (value && !Array.isArray(value)) {
-			setSelectedDate(value as Date);
-			sendMessage();
+			sendMessage(value as Date);
 		}
 	};
 
 	const handleUpdated = (newState: StateItem) => {
 		if (newState) {
+			console.log("got new state", newState);
 			setState(newState);
 		}
 	};
@@ -39,17 +42,13 @@ export const Dashboard: React.FC = () => {
 		socket.on("updated", handleUpdated);
 	}, [socket]);
 
-	const sendMessage = () => {
-		const message = {
+	const sendMessage = (date: Date) => {
+		socket.emit("update", {
 			created: new Date().getTime(),
 			type: "SET_DATE",
 			stateKey: "myApp",
-			payload: selectedDate.toISOString().slice(0, 10),
-		};
-
-		console.log("sending message", message);
-
-		socket.emit("update", message);
+			payload: date.toISOString().slice(0, 10),
+		});
 	};
 
 	return (
@@ -61,20 +60,6 @@ export const Dashboard: React.FC = () => {
 					showWeekNumbers
 					value={selectedDate}
 				/>
-				<button onClick={sendMessage}>UPDATE</button>
-				<SimpleCard>
-					<p className="text-gray-500">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-						sed do eiusmod tempor incididunt ut labore et dolore
-						magna aliqua. Ut endddim ad minim veniam, quis nostrud
-						exercitation ullamco laboris nisi ut aliquip ex ea
-						commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu
-						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-						non proident, sunt in culpa qui officia deserunt mollit
-						anim id est laborum.
-					</p>
-				</SimpleCard>
 			</div>
 		</div>
 	);
